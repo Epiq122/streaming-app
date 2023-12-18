5b307b37f10bea202e3b0d264439bd03a9d440ad
Robert Gleason <l2ob122@gmail.com>
2023-12-18 13:32:20 -0800

    created the sidebar and added features to open and close and work with mobile

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from "../navbar/logo";
 import { Search } from "@/app/(browse)/_components/navbar/search";
 import { Actions } from "@/app/(browse)/_components/navbar/actions";
 
-export default function NavBar() {
+export const Navbar = () => {
   return (
     <nav className="fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm">
       <Logo />
@@ -10,4 +10,4 @@ export default function NavBar() {
       <Actions />
     </nav>
   );
-}
+};
