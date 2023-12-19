4a11ddab395d51e34f0cedd4901faefdad415f00
Robert Gleason <l2ob122@gmail.com>
2023-12-18 19:07:23 -0800

    created logic for the reccomded list, created reuable skeletons, added logic to show users in side bar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,13 +1,17 @@
-import { Sidebar } from "./_components/sidebar";
+import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
 import { Navbar } from "./_components/navbar";
 import { Container } from "./_components/container";
+import { Suspense } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
       <div className="flex  h-full pt-20">
-        <Sidebar />
+        <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense>
+
         <Container>{children}</Container>
       </div>
     </>
